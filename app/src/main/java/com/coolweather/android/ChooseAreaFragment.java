dbabf2b93271ba4457b9094784c281a454d3394d
single-man <1975936508@qq.com>
2020-06-22 14:58:00 +0800

    cool

--- app/src/main/java/com/coolweather/android/ChooseAreaFragment.java
+++ app/src/main/java/com/coolweather/android/ChooseAreaFragment.java
@@ -1,9 +1,9 @@
 package com.coolweather.android;
 
+import android.app.Fragment;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
